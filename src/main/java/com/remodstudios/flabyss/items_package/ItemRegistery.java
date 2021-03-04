28b1b49f904a5820f753c5957082938fb856a76b
LePichu <ishat.prsnl65844@gmail.com>
2021-03-04 18:23:54 +0530

    HYPER CONCERN

--- src/main/java/com/remodstudios/flabyss/items_package/ItemRegistery.java
+++ src/main/java/com/remodstudios/flabyss/items_package/ItemRegistery.java
@@ -4,10 +4,7 @@ import com.remodstudios.flabyss.FlaringAbyssMain;
 import com.remodstudios.flabyss.blocks_package.BlocksRegistery;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -15,7 +12,7 @@ public class ItemRegistery {
     private static final ItemGroup GROUP = FabricItemGroupBuilder.build(new Identifier(FlaringAbyssMain.MOD_ID,"group"), () -> new ItemStack(BlocksRegistery.WILTSLATE));
 
     // "Items go here" - LePichu
-
+    public static final Item STYGIAN_IRON_SWORD = register(new StygianSword(ToolMaterials.IRON, 4, -2.4F, new Item.Settings().group(GROUP).maxCount(1)), "stygian_iron_sword");
 
     // "Block Items go blocky @ here" - LePichu, again
     public static final Item WILTSLATE = register(BlocksRegistery.WILTSLATE, "wiltslate", new Item.Settings().group(GROUP));
